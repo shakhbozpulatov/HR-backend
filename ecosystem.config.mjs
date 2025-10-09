64fcfbe3026d5d49e56605c8473b4db1db2be946
Shakhboz Pulatov <shakhbozpulatovdev@gmail.com>
2025-10-09 23:10:49 +0500

    ecosystem added

---
+++ ecosystem.config.mjs
@@ -0,0 +1,20 @@
+export default {
+  apps: [
+    {
+      name: 'hr-back',
+      script: './dist/main.js',
+      interpreter: 'node',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      merge_logs: true,
+      time: true,
+    },
+  ],
+};
